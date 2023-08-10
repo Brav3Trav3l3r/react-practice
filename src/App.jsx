a2e4071f6483101f01281a7fb882e12bc5ea52b7
Brav3Trav3l3r <yashgajbhiye10@gmail.com>
2023-08-10 23:45:14 +0530

    adding all files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import styles from "./App.module.css";
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <Counter />
+    </>
+  );
+}
+
+export default App;
