37a901bbc5c96ee83004fddf18338d6eb268796a
Brav3Trav3l3r <yashgajbhiye10@gmail.com>
2023-08-11 17:41:31 +0530

    clean up for react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import styles from "./App.module.css";
-import Counter from "./components/Counter";
-import Increment from './components/Increment'
 
 function App() {
   return (
     <>
       <h1>Hello</h1>
-      <Counter />
-      <Increment/>
     </>
   );
 }
--- src/components/Decrement.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useContext } from "react";
-import CounterContext from "../hooks/use-counter";
-
-export default function Decrement() {
-  const ctx = useContext(CounterContext);
-
-  return <button onClick={ctx.decrement}>Decrement</button>;
-}
